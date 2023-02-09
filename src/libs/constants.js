1fe4c603f1f9dc1eca703fb88a25fd43fc661eb6
Ephrem Demelash <demelashephrem@gmail.com>
2023-02-09 08:27:29 +0300

    feat: addedd a couple of constants for GAS fees

--- src/libs/constants.js
+++ src/libs/constants.js
@@ -1,12 +1,16 @@
 import { SupportedChainId, Token } from "@uniswap/sdk-core";
 import { FeeAmount } from "@uniswap/v3-sdk";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 export const POOL_FACTORY_CONTRACT_ADDRESS =
   "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 export const QUOTER_CONTRACT_ADDRESS =
   "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6";
+export const SWAP_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564";
+export const V3_SWAP_ROUTER_ADDRESS =
+  "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45";
 
 export const WETH_CONTRACT_ADDRESS =
   "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
@@ -17,9 +21,9 @@ export const WBTC_CONTRACT_ADDRESS =
 export const UNI_CONTRACT_ADDRESS =
   "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
 
-export const INFURA_GORLI_RPC = process.env.INFURA_GORLI_RPC;
-export const PRIVATE_KEY = process.env.PRIVATE_KEY;
-export const INFURA_RPC_ADDRESS = process.env.INFURA_RPC_ADDRESS;
+export const { INFURA_GORLI_RPC } = process.env;
+export const { PRIVATE_KEY } = process.env;
+export const { INFURA_RPC_ADDRESS } = process.env;
 
 // Currencies and Tokens for Goerli Testing purpose
 export const WETH_TOKEN = new Token(
@@ -58,3 +62,9 @@ export const CurrentConfig = {
     fee: FeeAmount.MEDIUM,
   },
 };
+
+// Transactions
+
+export const MAX_FEE_PER_GAS = 100000000000;
+export const MAX_PRIORITY_FEE_PER_GAS = 100000000000;
+export const TOKEN_AMOUNT_TO_APPROVE_FOR_TRANSFER = 2000;
