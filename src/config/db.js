4e58bfa7b7b0dd07429651c8c061b9dcd1dc85f2
Ephrem Demelash <demelashephrem@gmail.com>
2023-02-15 06:38:28 +0300

    feat: config to connect to mongodb

---
+++ src/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDb = () => {
+  try {
+    mongoose
+      .connect(process.env.MONGO_DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log("MongoDB Connected..."));
+
+    mongoose.set("debug", true);
+  } catch (err) {
+    console.error(err.message);
+    throw new Error("MongoDB Connection Failed");
+  }
+};
+
+export default connectDb;
