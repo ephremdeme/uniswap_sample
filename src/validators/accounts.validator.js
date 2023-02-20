252eaa11134ac48acb91ee07f5ad5e0ac05197b6
Ephrem Demelash <demelashephrem@gmail.com>
2023-02-20 17:26:33 +0300

    adds cryptor for encryptions

--- src/validators/accounts.validator.js
+++ src/validators/accounts.validator.js
@@ -8,7 +8,6 @@ const createAccountValidator = Joi.object({
 const updateAccountValidator = Joi.object({
   name: Joi.string().required(),
   privateKey: Joi.string().required(),
-  id: Joi.string().required(),
 });
 
 export { createAccountValidator, updateAccountValidator };
