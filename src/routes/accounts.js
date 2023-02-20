a3cd945ce874c587ae45f13e78f6c80178d5d388
Ephrem Demelash <demelashephrem@gmail.com>
2023-02-21 00:43:01 +0300

    feat: integrate account tokens CRUD

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,9 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 import express from "express";
 import mongoose from "mongoose";
+import { INFURA_GORLI_RPC } from "../libs/constants";
 import { decrypt, encrypt } from "../libs/encrypt";
 
 import Account from "../models/accounts";
+import Token from "../models/tokens";
+import Uniswap from "../uniswap";
+import { toReadableAmount } from "../libs/utils";
 import {
   createAccountValidator,
   updateAccountValidator,
@@ -31,6 +35,82 @@ accountRoute.get("/:id", async (req, res) => {
   if (!account) return res.status(404).json({ message: "Account not found" });
 
   return res.json({ ...account, privateKey: decrypt(account.privateKey) });
+});
+
+accountRoute.post("/:id/tokens", async (req, res) => {
+  const { id } = req.params;
+  const { token } = req.body;
+
+  if (!id) return res.status(400).json({ message: "Id is required" });
+  if (!token) return res.status(400).json({ message: "Token is required" });
+
+  const tokenFound = await Token.findById(token);
+  if (!tokenFound) return res.status(404).json({ message: "Token not found" });
+
+  const account = await Account.findById(id);
+  if (!account) return res.status(404).json({ message: "Account not found" });
+
+  // check if token already exists
+  const tokenExists = account.tokens.find((t) => t.toString() === token);
+  if (tokenExists)
+    return res.status(400).json({ message: "Token already exists" });
+
+  account.tokens.push(token);
+
+  await account.save();
+
+  return res.json({ message: "Token added successfully" });
+});
+
+// get account by id
+accountRoute.get("/:id/tokens", async (req, res) => {
+  const { id } = req.params;
+  if (!id) return res.status(400).json({ message: "Id is required" });
+
+  const account = await Account.findById(id).populate("tokens").lean();
+  if (!account) return res.status(404).json({ message: "Account not found" });
+
+  const uniswapClient = new Uniswap(
+    INFURA_GORLI_RPC,
+    decrypt(account.privateKey)
+  );
+  const tokens = await Promise.all(
+    account.tokens.map(async (token) => {
+      const [_token, tokenBalalance] = await uniswapClient.getTokenAndBalance(
+        token.address
+      );
+
+      return {
+        ..._token,
+        ...token,
+        balance: toReadableAmount(tokenBalalance, token.decimals).toString(),
+      };
+    })
+  );
+
+  return res.json(tokens);
+});
+
+// remove token from account
+accountRoute.delete("/:id/tokens/:tokenId", async (req, res) => {
+  const { id, tokenId } = req.params;
+  if (!id) return res.status(400).json({ message: "Id is required" });
+  if (!tokenId)
+    return res.status(400).json({ message: "Token id is required" });
+
+  const account = await Account.findById(id);
+  if (!account) return res.status(404).json({ message: "Account not found" });
+
+  // check if token already exists
+  const tokenExists = account.tokens.find((t) => t.toString() === tokenId);
+  if (!tokenExists)
+    return res.status(400).json({ message: "Token does not exists" });
+
+  account.tokens = account.tokens.filter((t) => t.toString() !== tokenId);
+
+  await account.save();
+
+  return res.json({ message: "Token removed successfully" });
 });
 
 // create account
@@ -62,17 +142,15 @@ accountRoute.put("/:id", async (req, res) => {
   }
 
   const privateKey = encrypt(unEncryptedKey);
-  const account = await Account.findOneAndUpdate(
+  await Account.updateOne(
     { _id: mongoose.Types.ObjectId(id) },
     { $set: { name, privateKey } },
     { new: true }
-  )
-    .lean()
-    .catch((err) => {
-      console.log("err => ", err);
-    });
-  console.log("account => ", account);
-  return res.json({ ...account, privateKey: decrypt(account.privateKey) });
+  ).catch((err) => {
+    console.log("err => ", err);
+  });
+
+  return res.json({ message: "Account updated successfully" });
 });
 
 // delete account
