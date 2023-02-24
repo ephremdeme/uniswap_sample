92f253dd86e851c6828fd54778a88f701b40be58
Ephrem Demelash <demelashephrem@gmail.com>
2023-02-24 14:40:43 +0300

    feat: add a catch to check and conttinue exec

--- src/liquidityChecker.js
+++ src/liquidityChecker.js
@@ -1,4 +1,3 @@
-
 import dotenv from "dotenv";
 
 import { INFURA_RPC_ADDRESS } from "./libs/constants";
@@ -44,50 +43,58 @@ dotenv.config();
         const { privateKey: _privateKey } = account;
         const privateKey = decrypt(_privateKey);
         const uniClient = new Uniswap(INFURA_RPC_ADDRESS, privateKey);
-        const positions = await uniClient.getLiquidityPositions();
 
-        await Promise.all(
-          liquidity.map(async (liq) => {
-            const { positionId, stopLoss } = liq;
-            console.log("Position ID => ", positionId);
-            const position = positions.find(
-              (pos) => parseInt(pos.id, 10) === positionId
-            );
+        try {
+          const positions = await uniClient.getLiquidityPositions();
 
-            if (!position) {
-              return;
-            }
+          await Promise.all(
+            liquidity.map(async (liq) => {
+              const { positionId, stopLoss } = liq;
+              console.log("Position ID => ", positionId);
+              const position = positions.find(
+                (pos) => parseInt(pos.id, 10) === positionId
+              );
 
-            const { token0, token1 } = position;
+              if (!position) {
+                return;
+              }
 
-            const rate = await uniClient.getExchangeRate(token0.id, token1.id);
+              const { token0, token1 } = position;
 
-            console.log("Rate => ", rate, stopLoss);
-            if (parseFloat(rate) >= parseFloat(stopLoss)) {
-              return;
-            }
+              const rate = await uniClient.getExchangeRate(
+                token0.id,
+                token1.id
+              );
 
-            const { depositedToken0, depositedToken1, withdrawnToken1 } =
-              position;
+              console.log("Rate => ", rate, stopLoss);
+              if (parseFloat(rate) >= parseFloat(stopLoss)) {
+                return;
+              }
 
-            const removeLiquidity = await uniClient.removeLiquidity(
-              positionId,
-              token0.id, 
-              token1.id,
-              depositedToken0,
-              depositedToken1
-            );
-            console.log("Removed Liquidity => ", removeLiquidity);
+              const { depositedToken0, depositedToken1, withdrawnToken1 } =
+                position;
 
-            // Swap the tokens to the previous token value after removing the liquidity
-            const swap = await uniClient.exchangeTokenOutPut(
-              token0.id,
-              token1.id,
-              depositedToken1 - withdrawnToken1
-            );
-            console.log("Swapped Tokens => ", swap);
-          })
-        );
+              const removeLiquidity = await uniClient.removeLiquidity(
+                positionId,
+                token0.id,
+                token1.id,
+                depositedToken0,
+                depositedToken1
+              );
+              console.log("Removed Liquidity => ", removeLiquidity);
+
+              // Swap the tokens to the previous token value after removing the liquidity
+              const swap = await uniClient.exchangeTokenOutPut(
+                token0.id,
+                token1.id,
+                depositedToken1 - withdrawnToken1
+              );
+              console.log("Swapped Tokens => ", swap);
+            })
+          );
+        } catch (error) {
+          console.log("Error => ", error);
+        }
       })
     );
   }, 3 * 60 * 1000);
